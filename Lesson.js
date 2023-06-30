20d3c742331db882a75ba6d2ff301ab288516c62
015 <anbschool0015@015s-Mac-mini.local>
2023-06-30 10:19:08 +0700

    Javascript

---
+++ Lesson.js
@@ -0,0 +1,96 @@
+// var x = 10;
+
+// function increment() {
+//   var x = 5;
+//   x++;
+//   console.log(x); // declare new x
+// }
+
+// increment();//output function
+// console.log(x);// first x
+
+// Define a function to calculate the average of a list of numbers
+// function calculateAverage(numbers) {
+//     var sum = 0; // Prepare a variable to keep track of the total sum
+//     for (var i = 0; i < numbers.length; i++) {
+//       // Go through each number in the list
+//       sum += numbers[i]; // Add the number to the total sum
+//     }
+//     var average = sum / numbers.length; // Calculate the average by dividing the sum by the number of elements
+//     return average; // Return the calculated average
+//   }
+  
+//   // Define a list of scores for a person
+//   var scores = [85, 90, 92, 88, 95];
+  
+//   // Use the calculateAverage function to find the average score
+//   var averageScore = calculateAverage(scores);
+  
+//   // Print the average score to the console
+//   console.log("The average score is: " + averageScore);
+  
+// var person = {
+//     name: "John",
+//     age: 25,
+//     address: {
+//       street: "123 Main St",
+//       city: "New York",
+//       country: "USA"
+//     }
+//   };
+  
+//   console.log(person.address.city); output= new york
+//   var fruits = ["apple", "banana", "orange", "grape"];
+
+// for (var i = 0; i < fruits.length; i++) {
+//   console.log(fruits[i]);
+// } fruits.length mean number inside fruits so Fruits{0,1,2,3} fruit = 4 
+
+// var numbers = [1, 2, 3, 4, 5];
+// var sum = 0;
+
+// for (var i = 0; i < numbers.length; i++) {
+//   sum += numbers[i];
+// }
+// console.log(sum);
+
+// // Initialize a variable 'number' with the value 10
+// var number = 10;
+
+// // Define a function 'addFive' that takes a parameter 'num'
+// function addFive(num) {
+//   // Increment the value of 'num' by 5
+//   num += 5;
+//   // Return the updated value of 'num'
+//   return num;
+// }
+
+// // Call the 'addFive' function with the argument 'number'
+// // The value of 'number' is passed to the function and temporarily assigned to 'num'
+// var result = addFive(number);
+
+// // Output the value of 'result' to the console
+// console.log(result);
+
+// // Define a function called 'calculateFactorial' that takes a number as a parameter
+// function calculateFactorial(num) {
+//     // Check if the number is 0 or 1
+//     if (num === 0 || num === 1) {
+//       // If the number is 0 or 1, return 1 (base case for factorial)
+//       return 1;
+//     } else {
+//       // If the number is neither 0 nor 1, recursively calculate the factorial
+//       // by multiplying the number with the factorial of num - 1
+//       return num * calculateFactorial(num - 1);
+//     }
+//   }
+  
+//   // Call the 'calculateFactorial' function with the argument 5
+//   var result = calculateFactorial(5);
+  
+//   // Output the value of 'result' to the console
+//   console.log(result);
+
+
+  
+
